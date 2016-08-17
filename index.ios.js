55534b1bc6cd4a2c73a604eea65c13c0502eb5a5
Zacck <zacck@Zaccks-MacBook-Pro.local>
2016-08-17 12:46:57 +0200

    worked on scrollview examples

--- index.ios.js
+++ index.ios.js
@@ -1,41 +1,28 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, Text, View, StyleSheet, ScrollView, TextInput } from 'react-native';
+import { AppRegistry, Image, Text, View, StyleSheet,ListView,  ScrollView, TextInput } from 'react-native';
 
 //This is a component sort of like an activity
 class HelloWorld extends Component {
+  //init the data for the list
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    //confgure a data store
+    const DataStore = new ListView.DataSource ({rowHasChanged: (r1, r2) => {r !== r2}});
+    this.state = {
+      dataSource: DataStore.cloneWithRows([
+        'nick', 'marike', 'paula','lee','jeff', 'tracy-lee', 'terri-anne'
+      ])
+    };
+
   }
   render(){
     return (
-      <ScrollView>
-        <Text style={{fontSize: 96}}>Scroll me pls!</Text>
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Text style={{fontSize: 96}}>If you like it</Text>
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Text style={{fontSize:96}}>Scrolling Down</Text>
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Text style={{fontSize:96}}>Whats the best ?</Text>
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Image source={require('./img/drink.png')} />
-        <Text style={{fontSize:80}}>React Native</Text>
-      </ScrollView>
+      <View style={{paddingTop: 22}}>
+        <ListView
+          dataSource = {this.state.dataSource}
+          renderRow = {{rowData => <Text>{rowData}</Text>}}
+        />
+      </View>
     );
   }
 }
